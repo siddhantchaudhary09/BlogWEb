82c9293641ca08626a48beff4d72fca66d84e088
siddhant <siddhantchaudhary096@gmail.com>
2024-03-26 23:31:16 +0530

    live bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "/all-post",
+        path: "/all-posts",
         element: (
           <Authlayout authentication>
             {" "}
