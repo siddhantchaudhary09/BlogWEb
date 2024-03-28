165b4694436e2fc1582ffe67e8a3b62c4961d915
siddhant <siddhantchaudhary096@gmail.com>
2024-03-28 15:13:18 +0530

    Editfeature

--- src/components/pages/Editpost.jsx
+++ src/components/pages/Editpost.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Postform from "../postform/Postform";
+import service from "../../Appwrite/Config";
+import Container from "../container/Container";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Editpost() {
-  return <div>Editpost</div>;
+  const [post, setPosts] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      service.getPost(slug).then((post) => {
+        if (post) {
+          setPosts(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <Postform post={post} />
+      </Container>
+    </div>
+  ) : null;
 }
 
 export default Editpost;
