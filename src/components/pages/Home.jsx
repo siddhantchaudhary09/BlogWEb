d33f47f7fa3b815ea6e658245657016b93fdf4c5
siddhant <siddhantchaudhary096@gmail.com>
2024-04-12 02:29:07 +0530

    UI fixed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,10 +3,12 @@ import service from "../../Appwrite/Config";
 import Container from "../container/Container";
 import { useSelector } from "react-redux";
 import Postcard from "../Postcard";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [posts, setPosts] = useState([]);
   const login = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
   useEffect(() => {
     service.getAllPosts().then((posts) => {
@@ -22,7 +24,19 @@ function Home() {
         <Container>
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold">Login to read posts</h1>
+              <h1 className="text-5xl font-bold">Login to read posts</h1>
+              <p className="font-bold text-2xl w-4/5 mx-auto py-5">
+                Embark on your blogging journey with ease! Begin crafting your
+                first captivating article on this user-friendly platform.
+              </p>
+              <div className="p-4 ">
+                <button
+                  onClick={() => navigate("/login")}
+                  className="font-semibold bg-white px-6 py-2 rounded-md hover:bg-gray-400 duration-200 "
+                >
+                  Get Started
+                </button>
+              </div>
             </div>
           </div>
         </Container>
